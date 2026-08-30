dd007b7b473072833924ece5c369ec45d878b7fd
virupakshayyavirakthamath1-oss <virupakshayyavirakthamath1@gmail.com>
2026-08-31 00:22:23 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import Flask, flash, jsonify, make_response, redirect, render_templat
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from database import get_db, init_db, now_iso, DB_PATH
-from nlp.checker import check_text
+from checker import check_text
 from nlp.dialect import detect_dialect
 from nlp.slang import get_slang_dictionary
 
